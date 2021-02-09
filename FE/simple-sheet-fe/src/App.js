7388f3e05dedc6df1416a3cc684c9bbe2819c17a
MichalObi <ndwz123@gmail.com>
2021-02-09 19:35:53 +0100

    Merge sheets and groups / add log wrapper

--- FE/simple-sheet-fe/src/App.js
+++ FE/simple-sheet-fe/src/App.js
@@ -3,45 +3,55 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [error, setError] = useState(null),
-        [isLoaded, setIsLoaded] = useState(false),
-        [items, setItems] = useState([]),
-        handleAjaxError = error => {
+  const useStateCallbackWrapper = (initilValue, callBack) => {
+    const [state, setState] = useState(initilValue);
+
+    useEffect(() => callBack(state), [state]);
+    return [state, setState];
+  };
+
+  const log = (key, state) => console.log(`${key}`, state);
+
+  const [error, setError] = useStateCallbackWrapper(null, state => log('error', state)),
+        [isLoaded, setIsLoaded] = useStateCallbackWrapper(false,  state => log('isLoaded', state)),
+        [sheetsAndGroups, setSheetsAndGroups] = useStateCallbackWrapper({}, state => log('sheetsAndGroups', state));
+
+  const handleAjaxError = error => {
           setIsLoaded(true);
           setError(error);
-
-          console.log('err', error);
         };
 
-        useEffect(() => {
-          fetch('/sheets/1')
-          .then(res => res.json())
-          .then(
-            res => {
-              console.log('sheets', res);
+    useEffect(() => {
+      fetch('/sheets/1')
+      .then(sheets => sheets.json())
+      .then(sheets => {
+          if (sheets) {
+            fetch(`/groups/${sheets.id}`)
+            .then(groups => groups.json())
+            .then(groups => {
+              const sheetsAndGroups = Object.assign({}, sheets, groups);
+              delete sheetsAndGroups.id;
 
-              fetch(`/groups/${res.id}`)
-              .then(res => res.json())
-              .then(res => {
-                setIsLoaded(true);
-                console.log('groups', res);
-              }, handleAjaxError)
+              setSheetsAndGroups(sheetsAndGroups);
+              setIsLoaded(true);
             }, handleAjaxError)
-        }, []);
+          }
+        }, handleAjaxError)
+    }, []);
 
-    if (error) {
-      return <p>error</p>;
-    } else if (!isLoaded) {
-      return <p>loading</p>;
-    } else {
-      return (
-        <div className="App">
-          <header className="App-header">
-            Simple Sheet
-          </header>
-        </div>
-      );
-    }
+  if (error) {
+    return <p>error</p>;
+  } else if (!isLoaded) {
+    return <p>loading</p>;
+  } else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Simple Sheet
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
