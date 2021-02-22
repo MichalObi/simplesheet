ccfc9e1eb3fb480c67e21c0487cd08acd7dbdf7f
MichalObi <ndwz123@gmail.com>
2021-02-22 19:19:09 +0100

    Move ajax call to Explore

--- FE/simple-sheet-fe/src/App.js
+++ FE/simple-sheet-fe/src/App.js
@@ -13,42 +13,6 @@ import ExploreSheets from './components/ExploreSheets';
 import Home from './components/Home';
 
 function App() {
-  const useStateCallbackWrapper = (initilValue, callBack) => {
-    const [state, setState] = useState(initilValue);
-
-    useEffect(() => callBack(state), [state]);
-    return [state, setState];
-  };
-
-  const log = (key, state) => console.log(`${key}`, state);
-
-  const [error, setError] = useStateCallbackWrapper(null, state => log('error', state)),
-        [isLoaded, setIsLoaded] = useStateCallbackWrapper(false,  state => log('isLoaded', state)),
-        [sheets, setSheets] = useStateCallbackWrapper({}, state => log('sheets', state));
-
-  const handleAjaxError = error => {
-          setIsLoaded(true);
-          setError(error);
-        };
-
-    // useEffect(() => {
-    //   fetch('/sheets/')
-    //   .then(sheets => sheets.json())
-    //   .then(sheets => {
-    //     setSheets(sheets);
-    //     setIsLoaded(true);
-    //     }, handleAjaxError)
-    // }, []);
-
-  useEffect(() => {
-    setIsLoaded(true);
-  }, []);
-
-  if (error) {
-    return <p>error</p>;
-  } else if (!isLoaded) {
-    return <p>loading</p>;
-  } else {
     return (
       <Router>
         <div className="App">
@@ -82,7 +46,6 @@ function App() {
         </Switch>
       </Router>
     );
-  }
 }
 
 export default App;
--- FE/simple-sheet-fe/src/components/ExploreSheets.js
+++ FE/simple-sheet-fe/src/components/ExploreSheets.js
@@ -1,5 +1,47 @@
+import React, {useState, useEffect} from 'react';
+
+
+
 function ExploreSheets() {
-  return <h2>Explore sheets</h2>;
+
+  const useStateCallbackWrapper = (initilValue, callBack) => {
+    const [state, setState] = useState(initilValue);
+
+    useEffect(() => callBack(state), [state]);
+    return [state, setState];
+  };
+
+  const log = (key, state) => console.log(`${key}`, state);
+
+  const [error, setError] = useStateCallbackWrapper(null, state => log('error', state)),
+        [isLoaded, setIsLoaded] = useStateCallbackWrapper(false,  state => log('isLoaded', state)),
+        [sheets, setSheets] = useStateCallbackWrapper({}, state => log('sheets', state));
+
+  const handleAjaxError = error => {
+          setIsLoaded(true);
+          setError(error);
+        };
+
+    useEffect(() => {
+      fetch('/sheets')
+      .then(sheets => sheets.json())
+      .then(sheets => {
+        setSheets(sheets);
+        setIsLoaded(true);
+        }, handleAjaxError)
+    }, []);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+
+  if (error) {
+    return <p>error</p>;
+  } else if (!isLoaded) {
+    return <p>loading</p>;
+  } else {
+    return <h2>Explore sheets</h2>;
+  }
 }
 
 export default ExploreSheets;
